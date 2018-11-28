5499492e6cc922d8ce67145bbd0101f9e4c5fb0d
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2018-11-28 20:21:31 +0530

    Delete AD_Schema_Indexed.ps1

--- AD_Schema_Indexed.ps1
+++
@@ -1,9 +0,0 @@
-## Check if schema attributes are indexed or not 
-
-$forest = [DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
-$Schema = $forest.schema 
-$Properties = $Schema.FindAllProperties()
-
-####Can filter more properties other than index values by running above lines of code and then typing $properties
-
-$Properties | select Name, IsIndexed | Format-Table -AutoSize

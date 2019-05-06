4c138cf6f41ef40f8f3be4635ccdf793a879b635
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2019-05-06 14:03:26 +0530

    Update Import-Module_Remote_Server.ps1

--- Import-Module_Remote_Server.ps1
+++ Import-Module_Remote_Server.ps1
@@ -6,3 +6,15 @@ $session = New-PSSession server1
 Invoke-Command -ScriptBlock {Import-Module activedirectory } -Session $session
 
 Import-PSSession -module ActiveDirectory -session $session
+
+
+===================================================================
+
+## Another Way (Not tested yet)
+
+$session = New-PSSession -ComputerName Server1
+
+Import-Module -PSSession $s -Name NetSecurity
+    
+Get-Command -Module NetSecurity -Name Get-*Firewall* # to check if module has been imported in the current session
+

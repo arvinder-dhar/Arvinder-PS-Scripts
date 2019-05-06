5e4aeb0a1092596d4be9d84c4336c54681983fc8
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2019-05-06 14:04:58 +0530

    Update Import-Module_Remote_Server.ps1

--- Import-Module_Remote_Server.ps1
+++ Import-Module_Remote_Server.ps1
@@ -14,7 +14,7 @@ Import-PSSession -module ActiveDirectory -session $session
 
 $session = New-PSSession -ComputerName Server1
 
-Import-Module -PSSession $s -Name NetSecurity
+Import-Module -PSSession $s -Name Activedirectory
     
-Get-Command -Module NetSecurity -Name Get-*Firewall* # to check if module has been imported in the current session
-
+Get-Command -Module Activedirectory -Name Get-AD* # to check if module has been imported in the current session and 
+                                                  # one can see the AD Module Cmdlets

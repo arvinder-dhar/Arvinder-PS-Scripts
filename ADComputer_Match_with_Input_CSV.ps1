dd04eb2be3744ae58cdf2782b33b1b390149b1f2
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2019-07-05 17:48:14 +0530

    Create ADComputer_Match_with_Input_CSV.ps1

---
+++ ADComputer_Match_with_Input_CSV.ps1
@@ -0,0 +1,51 @@
+#Author : Arvinder
+#Description : checks if the machine is availabe in the domain or not based on the csv file fetched as input source
+#limitations : script cannot fetch the machine info until domain name is mentioned against the machine column
+
+###### START  #####
+
+$servers = $null
+$result = @()
+$final_result = @()
+
+$servers = Import-Csv "C:\Users\$env:username\Desktop\Machines.csv"
+
+foreach ($server in $servers){
+
+$computer = $server.Device
+$domain = $server.domain
+
+try {
+
+$result = "" | select Device,Domain,Machine_in_Domain,Enabled
+
+$value = (Get-ADComputer -Identity $computer -Server $domain -Properties * -ErrorAction SilentlyContinue)
+$result.Device = $value.Name
+$result.Domain = $domain
+$result.Machine_in_Domain = "Present"
+$result.Enabled = $value.Enabled
+
+$result
+[array]$final_result += $result
+ 
+}
+
+catch {
+
+$result = "" | select Device,Domain,Machine_in_Domain,Enabled
+
+$result.Device = $computer
+$result.Domain = $domain
+$result.Machine_in_Domain = "Absent"
+$result.Enabled = "Not Applicable"
+
+$result
+[array]$final_result += $result
+
+}
+
+}
+
+$final_result | Export-Csv C:\Users\$env:username\Desktop\report.csv -NoTypeInformation
+
+###### END #####

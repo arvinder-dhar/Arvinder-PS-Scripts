c7b7f18aee03c5299ad4e13e1c0e93ecb6f7887d
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2020-10-21 12:46:09 +0530

    Create Ping_Domain.ps1

---
+++ Ping_Domain.ps1
@@ -0,0 +1,25 @@
+##Author: Arvinder
+### Get Ping status and domain name of Computers accounts fetched from file
+
+$report = @()
+$final_report = @()
+[array]$Data = Get-Content .\list.txt
+
+foreach ($machine in $Data) {
+
+    $report = "" | Select Hostname,ping_status,Domain_Name
+    $ping = Test-Connection -ComputerName $machine -Count 1 -Quiet
+    $fqdn = [System.Net.Dns]::GetHostByName($machine).hostname
+    $domain = $fqdn.Substring($fqdn.IndexOf(".") + 1)
+
+    $report.Hostname = $machine
+    $report.ping_status = $ping
+    $report.Domain_Name = $domain
+    
+    $report
+    [array]$final_report += $report
+
+
+}
+
+$final_report | Export-Csv servers.csv -NoTypeInformation
